6312b11f50c06557920dfc96c6ba3b3219cbd3d4
Semyon Perepelitsa <sema@sema.in>
2010-08-09 00:28:55 +0800

    adding auth common function and helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  helper_method :admin?
+  
+  protected
+  
+  def authorize
+    unless admin?
+      redirect_to login_path
+      false
+    end
+  end
+  
+  def admin?
+    session[:admin]
+  end
 end
