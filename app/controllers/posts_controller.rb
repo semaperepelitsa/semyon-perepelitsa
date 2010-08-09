ace41095a269e49461895f7910286ca037fa3f8f
Semyon Perepelitsa <sema@sema.in>
2010-08-09 15:54:05 +0800

    showing posts ordered by date

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.xml
   def index
-    @posts = Post.all
+    @posts = Post.order('created_at desc')
     
     @at_home = true
 
