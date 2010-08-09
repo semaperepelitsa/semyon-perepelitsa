6347a67c740b0307472d7e0c9fb84c761a1ff46e
Semyon Perepelitsa <sema@sema.in>
2010-08-09 16:09:00 +0800

    redirecting from root to blog

--- config/routes.rb
+++ config/routes.rb
@@ -54,7 +54,7 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "posts#index"
+  root :to => redirect('/stream')
 
   # See how all your routes lay out with "rake routes"
 
