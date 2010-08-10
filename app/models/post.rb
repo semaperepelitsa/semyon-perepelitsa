554969e334f4351652835dff2e69665c1dc02248
Semyon Perepelitsa <sema@sema.in>
2010-08-11 05:38:29 +0800

    adding scope for recent order and scopes for only published posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ActiveRecord::Base
   validates_presence_of :text
+  
+  scope :recent, order('created_at desc')
+  
+  scope :published, where(:published => true)
+  def self.published_unless(condition)
+    condition ? scoped : published
+  end
 end
