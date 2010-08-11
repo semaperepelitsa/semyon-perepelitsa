ee68ad35a0a6e9edfdb39a3bfd21da1541c87da2
Semyon Perepelitsa <sema@sema.in>
2010-08-11 23:51:54 +0800

    adding scope for ordering by update time

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ActiveRecord::Base
   validates_presence_of :text, :published
   
   scope :recent, order('created_at desc')
+  scope :last_updated, order('updated_at desc')
   
   scope :published, where(:published => true)
   def self.published_unless(condition)
