fff1b56f8c5a3b7f9b31546bb5e9f96f26e96f43
Semyon Perepelitsa <sema@sema.in>
2010-08-14 06:12:18 +0800

    removing validation of published field

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates_presence_of :text, :published
+  validates_presence_of :text
   
   scope :recent, order('created_at desc')
   scope :last_updated, order('updated_at desc')
