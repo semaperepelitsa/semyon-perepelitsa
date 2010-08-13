9f8a31225ba6cebb41297c2cf67619dcccb387fd
Semyon Perepelitsa <sema@sema.in>
2010-08-14 01:11:12 +0800

    removing title column from posts table

---
+++ db/migrate/20100813170858_remove_title_from_posts.rb
@@ -0,0 +1,9 @@
+class RemoveTitleFromPosts < ActiveRecord::Migration
+  def self.up
+    remove_column :posts, :title
+  end
+
+  def self.down
+    add_column :posts, :title, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100807093529) do
+ActiveRecord::Schema.define(:version => 20100813170858) do
 
   create_table "posts", :force => true do |t|
-    t.string   "title"
     t.text     "text"
     t.boolean  "published"
     t.datetime "created_at"
