dc19000cc49eecc98fd3936ca47a436ca4393b55
Semyon Perepelitsa <sema@sema.in>
2010-08-14 05:57:41 +0800

    making dynamic page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,12 @@ module ApplicationHelper
     BlueCloth.new(text).to_html.html_safe
   end
   
+  def title(str = nil)
+    if str
+      @page_title = str
+    elsif @page_title.nil?
+      @page_title = 'Поток мыслей'
+    end
+    @page_title
+  end
 end
