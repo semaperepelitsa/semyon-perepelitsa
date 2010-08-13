f629ba7cdad5e85e5266dad3dc4be533b0f202b2
Semyon Perepelitsa <sema@sema.in>
2010-08-14 06:15:14 +0800

    redirecting to drafts page if saved unpublished post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -52,7 +52,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to(posts_path, :notice => 'Post was successfully created.') }
+        format.html { redirect_to(@post.published? ? posts_path : unpublished_posts_path, :notice => 'Post was successfully created.') }
         format.xml  { render :xml => @post, :status => :created, :location => @post }
       else
         format.html { render :action => "new" }
