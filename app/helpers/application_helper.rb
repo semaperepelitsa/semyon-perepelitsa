be8914cea515c5b58e4b7dc438b05b3c8fd6911e
Semyon Perepelitsa <sema@sema.in>
2010-08-16 04:34:27 +0800

    moving delete tag ago from helper (some bug with haml)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,4 @@
 module ApplicationHelper
-  def delete_tag(path, name = 'Удалить')
-    capture form_tag(path, :method => :delete) do
-      submit_tag name, :class => 'delete'
-    end
-  end
-  
   def markdownize text
     BlueCloth.new(text).to_html.html_safe
   end
