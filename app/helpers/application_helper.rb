4aa6ba276f94e3f4756042c5286847c8b51cf180
Semyon Perepelitsa <sema@sema.in>
2010-08-18 07:45:49 +0800

    fixing yandex.metrika counter errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,8 +13,8 @@ module ApplicationHelper
   end
   
   def counter_tag
-    if env.production?
-      <<EOC
+    if Rails.env.production? && !admin?
+      html = <<EOC
 <!-- Yandex.Metrika -->
 <script src="//mc.yandex.ru/metrika/watch.js" type="text/javascript"></script>
 <div style="display:none;"><script type="text/javascript">
@@ -23,8 +23,7 @@ try { var yaCounter1309037 = new Ya.Metrika(1309037); } catch(e){}
 <noscript><div style="position:absolute"><img src="//mc.yandex.ru/watch/1309037" alt="" /></div></noscript>
 <!-- /Yandex.Metrika -->
 EOC
-    else
-      ''
+      html.html_safe
     end
   end
 end
