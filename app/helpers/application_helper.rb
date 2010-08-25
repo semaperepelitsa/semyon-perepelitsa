4df094e336854d33f2d2f6d6b3552c0f2170b301
Semyon Perepelitsa <sema@sema.in>
2010-08-25 14:26:40 +0800

    handle field error display in helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,6 +15,15 @@ module ApplicationHelper
     content_tag :h1, content.html_safe
   end
   
+  def error_for(field, model)
+    res = ''
+    human_field = model.class.human_attribute_name(field)
+    model.errors[field].each do |error|
+      res += content_tag :div, "#{human_field} #{error}", :class => 'error'
+    end
+    raw(res)
+  end
+  
   def counter_tag
     if Rails.env.production? && !admin?
       html = <<EOC
