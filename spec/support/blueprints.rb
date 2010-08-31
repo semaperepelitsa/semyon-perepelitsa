e2f83dc2d030400391c55d7db6114dc38e7d6262
Semyon Perepelitsa <sema@sema.in>
2010-08-31 20:15:22 +0800

    adding SN to text of Post blueprint

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -2,7 +2,7 @@ require 'machinist/active_record'
 
 Post.blueprint do
   title { "Post #{sn}" }
-  text { "Lorem ipsum dolor sit amet..." }
+  text { "Lorem ipsum dolor sit amet #{sn}..." }
   published { true }
 end
 
