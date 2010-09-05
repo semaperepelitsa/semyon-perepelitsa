2d3ef933e60f8b66ec158ee1b0b56f8e5e237dbc
Semyon Perepelitsa <sema@sema.in>
2010-09-05 11:32:22 +0800

    replacing direct timestamp processing with localization methods

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,8 @@
 module PostsHelper
   def compact_timestamp(timestamp)
-    content_tag(:span, Russian::strftime(timestamp, '%e %B'), :title => Russian::strftime(timestamp))
+    full = l(timestamp)
+    short = l(timestamp.to_date, :format => :short)
+    content_tag(:span, short, :title => full)
   end
 
 end
