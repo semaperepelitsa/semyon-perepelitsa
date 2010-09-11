8da9240515b2a4f3f6347a26f86939102e3e307c
Semyon Perepelitsa <sema@sema.in>
2010-09-11 14:33:36 +0800

    handle redirection to stream in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,7 @@
 class HomeController < ApplicationController
+  def index
+    respond_to do |f|
+      f.html { redirect_to posts_path }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,7 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => redirect('/stream')
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
