f5f775c9077f3fae62a38d6d8dc409a844c31135
Semyon Perepelitsa <sema@sema.in>
2010-09-11 21:15:40 +0800

    caching external openid xrds

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
       f.html { redirect_to posts_path }
       f.xrds do
         require 'open-uri'
-        render :text => open(APP_CONFIG['openid_xrds_url']).read
+        render :text => Rails.cache.fetch(:openid_xrds) { open(APP_CONFIG['openid_xrds_url']).read }
       end
     end
   end
