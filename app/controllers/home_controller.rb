7c1de0f7e84419ae35c12cf5f1d6b7d93d1d7c8a
Semyon Perepelitsa <sema@sema.in>
2010-09-13 19:34:30 +0800

    moving library requirement inside cache closure

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,10 @@ class HomeController < ApplicationController
     respond_to do |f|
       f.html { redirect_to posts_path }
       f.xrds do
-        require 'open-uri'
-        render :text => Rails.cache.fetch(:openid_xrds) { open(APP_CONFIG['openid_xrds_url']).read }
+        render :text => Rails.cache.fetch(:openid_xrds) do
+          require 'open-uri'
+          open(APP_CONFIG['openid_xrds_url']).read
+        end
       end
     end
   end
