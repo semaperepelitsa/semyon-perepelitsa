220b0584b1275b663c26b387a41420bbe0211283
Semyon Perepelitsa <sema@sema.in>
2010-12-05 00:17:23 +0700

    add db-seeding script

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+# encoding: utf-8
+
+require 'nokogiri'
+require 'open-uri'
+
+types = %w{astronomy physics philosophy}
+
+25.times do
+  post = Post.new
+  doc = Nokogiri::HTML(open("http://referats.yandex.ru/#{types.pick}.xml"))
+  h1 = doc.at_css("h1")
+  puts post.title = h1.content.match(/Тема: «(.+)»/)[1]
+  post.published = [true, false].pick
+  post.created_at = rand(30).days.ago + rand(10**5).seconds
+  post.updated_at = post.created_at + rand(Time.now - post.created_at).seconds if post.published
+  post.text = h1.parent.css('p').map { |t| t.content }.join("\n\n")
+  post.save!
+end
