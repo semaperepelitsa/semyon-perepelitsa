b28780eeeb88fbdc2a9a12977036963c72018dc9
Semyon Perepelitsa <sema@sema.in>
2011-01-18 17:32:21 +0700

    Create posts with #create! and block

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,14 @@ require 'open-uri'
 types = %w{astronomy physics philosophy}
 
 25.times do
-  post = Post.new
   doc = Nokogiri::HTML(open("http://referats.yandex.ru/#{types.pick}.xml"))
   h1 = doc.at_css("h1")
-  puts post.title = h1.content.match(/Тема: «(.+)»/)[1]
-  post.published = [true, false].pick
-  post.created_at = rand(30).days.ago + rand(10**5).seconds
-  post.updated_at = post.created_at + rand(Time.now - post.created_at).seconds if post.published
-  post.text = h1.parent.css('p').map { |t| t.content }.join("\n\n")
-  post.save!
+
+  Post.create! do |post|
+    puts post.title = h1.content.match(/Тема: «(.+)»/)[1]
+    post.published = [true, false].pick
+    post.created_at = rand(30).days.ago + rand(10**5).seconds
+    post.updated_at = post.created_at + rand(Time.now - post.created_at).seconds if post.published
+    post.text = h1.parent.css('p').map { |t| t.content }.join("\n\n")
+  end
 end
