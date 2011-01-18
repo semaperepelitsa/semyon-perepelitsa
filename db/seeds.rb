1850e9a4c9bee10b7a03c3bdb24a2166f7a48ef7
Semyon Perepelitsa <sema@sema.in>
2011-01-18 17:37:05 +0700

    Use shorthand syntax.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,6 +13,6 @@ types = %w{astronomy physics philosophy}
     post.published = [true, false].pick
     post.created_at = rand(30).days.ago + rand(10**5).seconds
     post.updated_at = post.created_at + rand(Time.now - post.created_at).seconds if post.published
-    post.text = h1.parent.css('p').map { |t| t.content }.join("\n\n")
+    post.text = h1.parent.css('p').map(&:content).join("\n\n")
   end
 end
