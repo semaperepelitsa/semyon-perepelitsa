0374a21dcb7b7d05c73b421ed35aea4f6a83b8cd
Semyon Perepelitsa <sema@sema.in>
2011-02-06 00:54:14 +0700

    Documentation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,18 +5,27 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  # A filter method for administrator actions.
   def authorize
     unless admin?
+      # Ensure the admin cookie is deleted
       admin! false
+
+      # Preserve the current URL to redirect back after login
       redirect_to login_path(:then => request.fullpath)
+
       false
     end
   end
 
+  # Returns true if the current user is an admin, false otherwise.
   def admin?
     !!session[:admin]
   end
 
+  # Set up admin flag session and cookie variables.
+  # Reset them if login is false.
+  # The cookie is unsafe. It is used only in client-side javascript for non-critic operations.
   def admin!(login = true)
     if login
       session[:admin] = true
