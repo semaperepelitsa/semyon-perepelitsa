78aed93cc089a1f3d8ae399b4a176d75761402bb
Semyon Perepelitsa <sema@sema.in>
2011-02-10 23:23:51 +0700

    Output HTML-valid timestamp

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,7 +4,7 @@ module PostsHelper
   def compact_timestamp(timestamp)
     full = l(timestamp)
     short = l(timestamp.to_date, :format => :short)
-    content_tag(:time, short, :title => full, :datetime => timestamp, :pubdate => :pubdate)
+    content_tag(:time, short, :title => full, :datetime => timestamp.xmlschema, :pubdate => :pubdate)
   end
 
   def timestamps_for(post)
