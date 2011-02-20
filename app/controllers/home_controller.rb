9e5597cf6797fa283e38aa01097035626159cd99
Semyon Perepelitsa <sema@sema.in>
2011-02-20 15:04:28 +0700

    Stupid bug. OpenID must work now.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,11 @@ class HomeController < ApplicationController
     respond_to do |f|
       f.html { redirect_to posts_path }
       f.xrds do
-        render :text => Rails.cache.fetch(:openid_xrds) do
+        xrds = Rails.cache.fetch(:openid_xrds) do
           require 'open-uri'
           open(APP_CONFIG['openid_xrds_url']).read
         end
+        render :text => xrds
       end
     end
   end
