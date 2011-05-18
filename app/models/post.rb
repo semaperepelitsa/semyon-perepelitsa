4c7b23f691d0227995a268f5ac147f023641e4ac
Semyon Perepelitsa <sema@sema.in>
2011-05-18 12:46:44 +0800

    string to symbol

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ActiveRecord::Base
   include Publication
   include FriendlyURL
 
-  validates_presence_of :text, :if => "published?"
+  validates_presence_of :text, :if => :published?
 
   scope :recent, order('published_at desc')
   scope :last_updated, order('updated_at desc')
